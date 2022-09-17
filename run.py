b0ef3e0b86c81ec7986afbfbac9838a6f363de2e
katkapsasky <kat_kapsasky@hotmail.com>
2022-09-17 15:05:53 +0000

    Add method to print secret word to console

--- run.py
+++ run.py
@@ -19,5 +19,22 @@ class Hangman():
         Method to take a letter and return a list
         with the secret word's indexes
         """
-        return [i for i, char in enumerate(self.secret_word) if letter == char] 
+        return [i for i, char in enumerate(self.secret_word) if letter == char]
+    
+    def invalid_input(self, input_):
+        """
+        Method to validate if the user input is correct
+        Checks if the user inputs a number or more than one character
+        """
+        return input_.isdigit() or (input_.isalpha() and len(input_) > 1)
+
+    def print_game_status(self):
+        """
+        Method to print the secret word with the letters guessed
+        and remaining blank spaces
+        """
+        print('\n')
+        print('\n'.join(HANGMAN[:self.wrong_guess]))
+        print('\n')
+        print(' '.join(self.progress))
 
