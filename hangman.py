49e015fd2382fc111735599945cb5e3bb74e5ad5
katkapsasky <kat_kapsasky@hotmail.com>
2022-09-17 14:38:40 +0000

    Import random module and hangman and words files

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
-# Code to create hangman taken from 
+# Code to create hangman taken from
 # https://gist.github.com/MiltonLn/72f4342ee938bf45881939e43b3230dc
 HANGMAN = [
     '________',
--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Module for generating random integers
+import random
+import hangman
+import words
+
+
+class Hangman():
+    """
+    Game class with methods
+    """
+    def __init__(self, secret_word):
+        self.wrong_guess = 0
+        self.secret_word = secret_word
+        self.progress = list('_' * len(self.secret_word))
