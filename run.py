22c3bf30479dd67d978ae22e28a79c16b2e681cd
katkapsasky <kat_kapsasky@hotmail.com>
2022-09-17 15:02:47 +0000

    Add method to find letters in secret word

--- run.py
+++ run.py
@@ -13,4 +13,11 @@ class Hangman():
         self.wrong_guess = 0
         self.secret_word = secret_word
         self.progress = list('_' * len(self.secret_word))
+    
+    def find_secret_word_letters(self, letter):
+        """
+        Method to take a letter and return a list
+        with the secret word's indexes
+        """
+        return [i for i, char in enumerate(self.secret_word) if letter == char] 
 
